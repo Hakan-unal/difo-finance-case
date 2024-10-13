234f659e0346c1cde73331b4d4874137f1cb9bea
Con Sınov <hakanunal_1996@hotmail.com>
2024-10-13 13:33:30 +0300

    updated

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,28 +4,32 @@ const { Paragraph, Text } = Typography;
 
 const Home = (props: any) => {
   return (
-    <Card className="home-card">
-      <Row>
-        <Col span={24} md={16}>
-          <Paragraph strong className="home-card-paragraph-1">
-            <Text  className="home-card-paragraph-text"> Difo Launchpad </Text>
-            is a new generation hybrid launch ramp that brings together the
-            valuable features of both centralized
-            <Text className="home-card-paragraph-text"> IEO </Text>
-            platforms and decentralized
-            <Text className="home-card-paragraph-text"> IDO </Text>
-            platforms
-          </Paragraph>
-          <Paragraph className="home-card-paragraph-2">
-            Gain early access to promising ideas of tomorrow with Difo
-            Launchpad.
-          </Paragraph>
-        </Col>
-        <Col span={24} md={8}>
-          <Image  preview={false} src={homeCardImage} />
-        </Col>
-      </Row>
-    </Card>
+    <Row>
+      <Card className="home-card">
+        <Row gutter={[12, 36]}>
+          <Col span={24} order={1} md={{span:16,order:0}}>
+            <Paragraph strong className="home-card-paragraph-1">
+              <Text className="home-card-paragraph-text"> Difo Launchpad </Text>
+              is a new generation hybrid launch ramp that brings together the
+              valuable features of both centralized
+              <Text className="home-card-paragraph-text"> IEO </Text>
+              platforms and decentralized
+              <Text className="home-card-paragraph-text"> IDO </Text>
+              platforms
+            </Paragraph>
+            <Paragraph className="home-card-paragraph-2">
+              Gain early access to promising ideas of tomorrow with Difo
+              Launchpad.
+            </Paragraph>
+          </Col>
+          <Col span={24} order={0} md={{span:8,order:1}} >
+            <Image preview={false} src={homeCardImage} />
+          </Col>
+        </Row>
+      </Card>
+
+
+    </Row>
   );
 };
 
